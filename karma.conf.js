0c5600db06b2da8a38cba8b44941d0a1aa17c104
Imre Tabur <imre.tabur@eesti.ee>
2021-11-22 16:05:27 +0200

    Karma config updated.

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,6 @@ module.exports = function (config) {
         plugins: [
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
-            require('karma-firefox-launcher'),
             require('karma-jasmine-html-reporter'),
             require('karma-coverage'),
             require('@angular-devkit/build-angular/plugins/karma')
@@ -38,7 +37,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: [/*'Chrome',*/ 'Firefox'],
+        browsers: ['Chrome'],
         singleRun: false,
         restartOnFileChange: true
     });
