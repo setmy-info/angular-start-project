c4b229f38aed760d302cba37f13417e99254741a
Imre Tabur <imre.tabur@mail.ee>
2026-07-06 00:21:17 +0300

    Save state

--- packages/angular-start-project-library/src/models/menuModel.js
+++ packages/angular-start-project-library/src/models/menuModel.js
@@ -1,8 +1,8 @@
 const menuItems = [
     {id: 1, path: '/', label: 'Home', icon: 'home', translationKey: 'menu.home'},
-    {id: 2, path: '/about', label: 'About', icon: 'info', translationKey: 'menu.about'},
+    {id: 2, path: '/articles', label: 'Articles', icon: 'library_books', translationKey: 'menu.articles'},
     {id: 3, path: '/contact', label: 'Contact', icon: 'contact_page', translationKey: 'menu.contact'},
-    {id: 4, path: '/terms', label: 'Terms of use', icon: 'gavel', translationKey: 'menu.terms'}
+    {id: 4, path: '/terms', label: 'Terms of use', icon: 'gavel', translationKey: 'menu.terms', header: false}
 ];
 
 module.exports = {default: menuItems};
