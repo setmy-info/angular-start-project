42f5dc78863a5d328bf716c71854cf389e4095ff
Imre Tabur <imre.tabur@mail.ee>
2026-07-05 22:16:19 +0300

    First portion fixes.

--- packages/angular-start-project-library/src/models/menuModel.js
+++ packages/angular-start-project-library/src/models/menuModel.js
@@ -1,7 +1,8 @@
 const menuItems = [
     {id: 1, path: '/', label: 'Home', icon: 'home', translationKey: 'menu.home'},
     {id: 2, path: '/about', label: 'About', icon: 'info', translationKey: 'menu.about'},
-    {id: 3, path: '/contact', label: 'Contact', icon: 'contact_page', translationKey: 'menu.contact'}
+    {id: 3, path: '/contact', label: 'Contact', icon: 'contact_page', translationKey: 'menu.contact'},
+    {id: 4, path: '/terms', label: 'Terms of use', icon: 'gavel', translationKey: 'menu.terms'}
 ];
 
 module.exports = {default: menuItems};
